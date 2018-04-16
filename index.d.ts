cc691b045f665fb41d2b7513aa656e3ed464636f
Steven Job <steven_job@live.com>
2018-04-16 22:17:15 +0100

    remove generated file

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type ActionHandlers<State, ActionType extends string, Action extends {
-    type: string;
-}> = {
-    readonly [P in ActionType]?: (state: State, action: Action) => State;
-};
-export declare function createReducer<State, ActionType extends string, Action extends {
-    type: ActionType;
-}>(initialState: State, actionHandlers: ActionHandlers<State, ActionType, Action>): (state: State | undefined, action: Action) => State;
-export default createReducer;
